f54ce53c97a5f4f2fbf586584a49c61dbe946df1
Wahaj Rashid <wahaj1020@gmail.com>
2026-02-06 08:12:10 +0000

    1.Two Sum

---
+++ 1. TwoSum.js
@@ -0,0 +1,19 @@
+console.log("Two Sum Solution");
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+};
+
+console.log(twoSum([1, 1, 2, 3, 4, 5], 2));

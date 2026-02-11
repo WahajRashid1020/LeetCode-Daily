e09a8a62a03bb513fb9c21c89d17cdfb888eb102
Wahaj Rashid <wahaj1020@gmail.com>
2026-02-11 04:00:05 +0000

    statepone

--- patternone_sliding_window.js
+++ patternone_sliding_window.js
@@ -1,3 +1,13 @@
+// Following are some ways you can identify that the given problem might require a sliding window:
+
+// The problem input is a linear data structure such as a linked list, array, or string
+// You’re asked to find the longest/shortest substring, subarray, or a desired value
+// Common problems you use the sliding window pattern with:
+
+// Maximum sum subarray of size ‘K’ (easy)
+// Longest substring with ‘K’ distinct characters (medium)
+// String anagrams (hard)
+
 // ONE TEMPLATE (MENTAL MODEL)
 // let left = 0
 // let state = new Set() | sum | map
